5da95620a7dbed3c1f7ced79e1ce583551556880
levy <levy9527@users.noreply.github.com>
2020-07-14 15:48:00 +0800

    chore: add netlify.sh

---
+++ netlify.sh
@@ -0,0 +1,15 @@
+echo "is netlify: $NETLIFY"
+echo "in branch: $BRANCH"
+
+if [ "$NETLIFY" != "true" ]
+then
+  echo "this script only runs in netlify, bye"
+  exit 1
+fi
+
+if [ "$BRANCH" != "dev" ]
+then
+  yarn doc
+else
+  echo "this script only runs in dev branch, bye"
+fi
