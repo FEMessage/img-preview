9e172112cb06a875729e36c79f29826041a20f06
levy <levy9527@users.noreply.github.com>
2020-07-14 16:50:48 +0800

    Update netlify.sh

--- netlify.sh
+++ netlify.sh
@@ -1,5 +1,11 @@
 echo "is netlify: $NETLIFY"
 echo "in branch: $BRANCH"
+echo "REPOSITORY_URL: $REPOSITORY_URL"
+echo "HEAD: $HEAD"
+echo "COMMIT_REF: $COMMIT_REF"
+echo "CACHED_COMMIT_REF: $CACHED_COMMIT_REF"
+echo "INCOMING_HOOK_TITLE: $INCOMING_HOOK_TITLE"
+echo "REPOSITORY_URL: $REPOSITORY_URL"
 
 if [ "$NETLIFY" != "true" ]
 then
